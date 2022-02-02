7e3c531d852ad93818f58205f53b6defe27ec237
yaguratake <66196068+yaguratake@users.noreply.github.com>
2022-02-03 02:27:38 +0900

    Add files via upload

---
+++ charapage/chara.js
@@ -0,0 +1,30 @@
+var positionY;					/* スクロール位置のY座標 */
+var STORAGE_KEY = "scrollY";	/* ローカルストレージキー */
+
+/*
+ * checkOffset関数: 現在のスクロール量をチェックしてストレージに保存
+ */
+function checkOffset(){
+	positionY = window.pageYOffset;
+	localStorage.setItem(STORAGE_KEY, positionY);
+}
+
+/*
+ * 起動時の処理
+ *
+ *		ローカルストレージをチェックして前回のスクロール位置に戻す
+ */
+window.addEventListener("load", function(){
+	// ストレージチェック
+	positionY = localStorage.getItem(STORAGE_KEY);
+
+	// 前回の保存データがあればスクロールする
+	if(positionY !== null){
+		scrollTo(0, positionY);
+	}
+
+	// スクロール時のイベント設定
+	window.addEventListener("scroll", checkOffset, false);
+});
+
+//参考にさせていただいたサイト➡【https://torisky.com/】
